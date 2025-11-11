30da0f1dcf3debe598aed0963276ff5f3ed4cb26
Dawood <muhammaddawoodtahir@gmail.com>
2025-11-11 23:06:16 +0000

    blogs view on homepage

--- components/sections/Blog1.tsx
+++ components/sections/Blog1.tsx
@@ -35,14 +35,6 @@ export default async function Blog1() {
             <h3 className="ds-3 mt-3 mb-3">Our Latest Articles</h3>
             <span className="fs-5 fw-medium">Explore the insights and trends shaping our industry</span>
           </div>
-          <div className="col-12 col-md-6 mt-3 mt-md-0">
-            <Link href="/blogs" className="ms-md-5 fw-bold text-primary">
-              See all articles
-              <svg className="ms-2" xmlns="http://www.w3.org/2000/svg" width={24} height={14} viewBox="0 0 24 14" fill="none">
-                <path className="fill-dark" d="M17.4177 0.417969L16.3487 1.48705L21.1059 6.24429H0V7.75621H21.1059L16.3487 12.5134L17.4177 13.5825L24 7.0002L17.4177 0.417969Z" fill="black" />
-              </svg>
-            </Link>
-          </div>
         </div>
 
         <div className="row">
@@ -62,13 +54,11 @@ export default async function Blog1() {
                 <div className="card-body p-0 bg-white">
                   <Link
                     href={`/blogs?category=${encodeURIComponent(b.category?.title ?? "general")}`}
-                    className="bg-primary-soft position-relative z-1 d-inline-flex rounded-pill px-3 py-2 mt-3"
+                    className="position-relative z-1 d-inline-flex mt-3"
                   >
-                    <span className="tag-spacing fs-7 fw-bold text-linear-2 text-uppercase">
-                      {b.category?.title ?? "General"}
-                    </span>
+                       <h6 className="my-3">{b.title}</h6>
+              
                   </Link>
-                  <h6 className="my-3">{b.title}</h6>
                 </div>
 
                 <Link href={`/blog/${b.slug}`} className="position-absolute bottom-0 start-0 end-0 top-0 z-0" />
