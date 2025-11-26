89d2b326a3662feb27059870f0fddee8070f695a
Dawood <muhammaddawoodtahir@gmail.com>
2025-11-26 10:18:30 +0000

    Google Tag Manager Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,13 +12,13 @@ import "/public/assets/css/main.css"
 import "@/node_modules/react-modal-video/css/modal-video.css"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
-
 import type { Metadata } from "next"
 import Script from "next/script"
 
 export const metadata: Metadata = {
   title: "My Yoga Network: Yoga Teachers for Hotels, Resorts, and Healthcare",
-  description: "My Yoga Network provides certified yoga teachers for hotels, resorts, healthcare facilities, and private practices, promoting wellness for all. Contact today!",
+  description:
+    "My Yoga Network provides certified yoga teachers for hotels, resorts, healthcare facilities, and private practices, promoting wellness for all. Contact today!",
 }
 
 export default function RootLayout({
@@ -34,8 +34,52 @@ export default function RootLayout({
           href="https://assets.calendly.com/assets/external/widget.css"
           rel="stylesheet"
         />
+
+        {/* Google Analytics (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-9R0KB4WPDK"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9R0KB4WPDK');
+          `}
+        </Script>
+
+        {/* Google Tag Manager */}
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`
+            (function (w, d, s, l, i) {
+              w[l] = w[l] || [];
+              w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+              });
+              var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+              j.async = true;
+              j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+              f.parentNode.insertBefore(j, f);
+            })(window, document, 'script', 'dataLayer', 'GTM-5QXB8XWL');
+          `}
+        </Script>
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5QXB8XWL"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         {children}
 
         {/* Calendly JS */}
@@ -43,7 +87,8 @@ export default function RootLayout({
           src="https://assets.calendly.com/assets/external/widget.js"
           strategy="afterInteractive"
         />
-         <SpeedInsights />
+
+        <SpeedInsights />
       </body>
     </html>
   )
